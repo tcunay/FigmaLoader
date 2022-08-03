c20291aa81ecaa15ad60125b5808535b1601315f
Gevorg <gulkanyangk@mail.ru>
2022-08-03 22:07:26 +0300

    added create prefab logic

--- Assets/Scripts/FigmaAutoLayout.cs
+++ Assets/Scripts/FigmaAutoLayout.cs
@@ -1,6 +1,8 @@
 using System;
 using System.IO;
 using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,8 +13,9 @@ namespace FigmaLoader
         private const string Token = "figd_Niah9mA9VNwIB7j_dqVQ8V8kiqs5NBRDqMdHM1Le";
         private const string URL = "https://api.figma.com/v1/files/";
         private const string FigmaToken = "X-Figma-Token";
-        
-        private string _fileKey = "tHYnGngWT4FBQH3gfAlZjP";
+
+        private File _file;
+        private string _fileKey = "n94eWohksh0b0RxB5L90zZ";
 
         [MenuItem("FigmaAutoLayout/Show")]
         public static void ShowWindow()
@@ -28,6 +31,7 @@ namespace FigmaLoader
             if (GUILayout.Button("Get file"))
             {
                 GetFile();
+                CreatePrefab();
             }
         }
 
@@ -53,7 +57,63 @@ namespace FigmaLoader
             }
             
             Debug.Log(json);
+            _file = JsonConvert.DeserializeObject<File>(json);
             response.Close();
         }
+
+        private void CreatePrefab()
+        {
+            Layer frame = _file.Document.Children[0].Children[0];
+
+            GameObject frameObject = new GameObject {name = frame.Name};
+            RectTransform frameRectTransform = frameObject.AddComponent<RectTransform>();
+            
+            frameRectTransform.pivot = new Vector2(0, 1);
+            frameRectTransform.anchorMax = new Vector2(0, 1);
+            frameRectTransform.anchorMin = new Vector2(0, 1);
+            frameRectTransform.anchoredPosition = Vector2.zero;
+            frameRectTransform.sizeDelta =
+                new Vector2(frame.AbsoluteBoundingBox.Width, frame.AbsoluteBoundingBox.Height);
+
+            foreach (var child in frame.Children)
+            {
+                CreateLayer(child, frame, frameRectTransform);
+            }
+            
+            if (!Directory.Exists("Assets/Prefabs"))
+                AssetDatabase.CreateFolder("Assets", "Prefabs");
+            string localPath = "Assets/Prefabs/" + frame.Name + ".prefab";
+            
+            PrefabUtility.SaveAsPrefabAsset(frameObject, localPath);
+
+        }
+
+        private void CreateLayer(Layer layer, Layer frame, Transform parent)
+        {
+            GameObject gameObject = new GameObject {name = frame.Name};
+            RectTransform rectTransform = gameObject.AddComponent<RectTransform>();
+
+            var x = layer.AbsoluteBoundingBox.X - frame.AbsoluteBoundingBox.X;
+            var y = frame.AbsoluteBoundingBox.Y - layer.AbsoluteBoundingBox.Y;
+            
+            rectTransform.pivot = new Vector2(0, 1);
+            rectTransform.anchorMax = new Vector2(0, 1);
+            rectTransform.anchorMin = new Vector2(0, 1);
+            rectTransform.anchoredPosition = new Vector2(x, y);
+            rectTransform.sizeDelta =
+                new Vector2(layer.AbsoluteBoundingBox.Width, layer.AbsoluteBoundingBox.Height);
+            
+            rectTransform.SetParent(parent, true);
+
+            if (layer.Children == null || layer.Children.Length == 0)
+                return;
+
+            foreach (var child in layer.Children)
+            {
+                CreateLayer(child, frame, rectTransform);
+            }
+        }
+
+        private string GetPrefabPath(string name) => $"Assets/{name}.prefab";
     }
 }
---
+++ Assets/Scripts/File.cs
@@ -0,0 +1,42 @@
+﻿namespace FigmaLoader
+{
+    public class File
+    {
+        public Document Document;
+    }
+
+    public class Document
+    {
+        public Page[] Children;
+    }
+
+    public class Page
+    {
+        public Layer[] Children;
+    }
+
+    public class Layer
+    {
+        public FigmaType Type;
+        public string Name;
+        public Box AbsoluteBoundingBox;
+        public Layer[] Children;
+    }
+
+    public enum FigmaType
+    {
+        Document,
+        Canvas,
+        Frame,
+        Rectangle,
+        Vector
+    }
+
+    public class Box
+    {
+        public float X;
+        public float Y;
+        public float Width;
+        public float Height;
+    }
+}
